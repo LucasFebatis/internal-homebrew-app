14ae690145db5e2ff31788e4d71ae391f50796a8
Lucas Batista <lfebatis@gmail.com>
2020-01-09 16:44:27 -0300

    virgula

--- internal-homebrew-app.rb
+++ internal-homebrew-app.rb
@@ -1,7 +1,7 @@
 class InternalHomebrewApp < Formula
   desc "A command line tool to retrieve local weather"
   homepage "https://github.com/LucasFebatis/internal-homebrew-app"
-  url "https://github.com/LucasFebatis/internal-homebrew-app/blob/master/archive/main.tar.gz" :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/LucasFebatis/internal-homebrew-app/blob/master/archive/main.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "3074ad54256717041c9006a874988d04a30a92941cac796507889538612c47b9"
   version "1.0.0"
 
