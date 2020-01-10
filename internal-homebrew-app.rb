b26d0cd572444040a1fe107cd9180cd6ea02195b
Lucas Batista <lfebatis@gmail.com>
2020-01-10 10:19:20 -0300

    UP

--- internal-homebrew-app.rb
+++ internal-homebrew-app.rb
@@ -3,7 +3,7 @@ require_relative "./custom_download_strategy.rb"
 class InternalHomebrewApp < Formula
   desc "A command line tool to retrieve local weather"
   homepage "https://github.com/LucasFebatis/internal-homebrew-app"
-  url "https://github.com/LucasFebatis/internal-homebrew-app/blob/master/archive/main.tar.gz", :using => CustomGitHubPrivateRepositoryDownloadStrategy
+  url "https://github.com/LucasFebatis/internal-homebrew-app/blob/master/archive/main.tar.gz", :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "3074ad54256717041c9006a874988d04a30a92941cac796507889538612c47b9"
   version "1.0.0"
 
