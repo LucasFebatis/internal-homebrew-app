5ffae77af272cc67176bf384fea68b9201340933
Lucas Batista <lfebatis@gmail.com>
2020-01-10 15:10:55 -0300

    up

--- internal-homebrew-app.rb
+++ internal-homebrew-app.rb
@@ -1,29 +1,8 @@
-class MyDownloadStrategy < GitDownloadStrategy
-  def fetch
-    # downloads output to `temporary_path`
-    require 'uri'
-    require 'net/http'
-
-    url = URI("https://github.com/LucasFebatis/internal-homebrew-app/blob/master/archive/main.tar.gz")
-
-    http = Net::HTTP.new(url.host, url.port)
-
-    request = Net::HTTP::Get.new(url)
-    request["Authorization"] = 'token 95fc9f932befdaa34fccc78e46b65a2c8c29165e'
-    request["Accept-Encoding"] = 'gzip, deflate'
-    request["Connection"] = 'keep-alive'
-    request["cache-control"] = 'no-cache'
-
-    response = http.request(request)
-    puts response.read_body
-
-  end
-end
 
 class InternalHomebrewApp < Formula
   desc "A command line tool to retrieve local weather"
   homepage "https://github.com/LucasFebatis/internal-homebrew-app.git"
-  url "https://github.com/LucasFebatis/internal-homebrew-app/blob/master/archive/main.tar.gz", :using => MyDownloadStrategy
+  url "https://github.com/LucasFebatis/internal-homebrew-app/releases/download/v0.0.1/archive/main.tar.gz", :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "3074ad54256717041c9006a874988d04a30a92941cac796507889538612c47b9"
   version "1.0.0"
 
