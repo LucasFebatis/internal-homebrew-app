6b9f37fedb2ae6559a013f0bf77708b58eeaa716
Lucas Batista <lfebatis@gmail.com>
2020-01-10 13:51:32 -0300

    teste

--- internal-homebrew-app.rb
+++ internal-homebrew-app.rb
@@ -1,9 +1,9 @@
 require_relative "./custom_download_strategy.rb"
 
-class PrivaterTap < Formula
+class InternalHomebrewApp < Formula
   desc "A command line tool to retrieve local weather"
-  homepage "https://bitbucket.org/lucasbatistafcamara/private-tap"
-  url "https://bitbucket.org/lucasbatistafcamara/private-tap/src/master/archive/main.tar.gz/teste", :using => :git
+  homepage "https://github.com/LucasFebatis/internal-homebrew-app.git"
+  url "https://github.com/LucasFebatis/internal-homebrew-app/blob/master/archive/main.tar.gz", :using => :git
   sha256 "3074ad54256717041c9006a874988d04a30a92941cac796507889538612c47b9"
   version "1.0.0"
 
