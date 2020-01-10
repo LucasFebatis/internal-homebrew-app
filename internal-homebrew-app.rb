728c4d96cc1e285723712d41760b637b36e04c58
Lucas Batista <lfebatis@gmail.com>
2020-01-10 15:12:30 -0300

    up

--- internal-homebrew-app.rb
+++ internal-homebrew-app.rb
@@ -1,4 +1,6 @@
 
+require "./custom_download_strategy"
+
 class InternalHomebrewApp < Formula
   desc "A command line tool to retrieve local weather"
   homepage "https://github.com/LucasFebatis/internal-homebrew-app.git"
