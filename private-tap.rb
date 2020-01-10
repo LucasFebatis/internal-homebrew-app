ec266d7e6240afb702f85da31aeea36075a12de1
Lucas Batista <lfebatis@gmail.com>
2020-01-10 13:42:43 -0300

    teste

--- private-tap.rb
+++ private-tap.rb
@@ -3,7 +3,7 @@ require_relative "./custom_download_strategy.rb"
 class PrivateTap < Formula
   desc "A command line tool to retrieve local weather"
   homepage "https://bitbucket.org/lucasbatistafcamara/private-tap"
-  url "https://bitbucket.org/lucasbatistafcamara/private-tap/src/master/archive/main.tar.gz", :using => CustomGitPrivateRepositoryDownloadStrategy
+  url "https://bitbucket.org/lucasbatistafcamara/private-tap/src/master/archive/main.tar.gz", :using => :git
   sha256 "3074ad54256717041c9006a874988d04a30a92941cac796507889538612c47b9"
   version "1.0.0"
 
